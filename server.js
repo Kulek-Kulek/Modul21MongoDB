5461fcb55397b6637381f031f04afe950a422412
Krzysztof Ługowski <k.lugowski@yahoo.com>
2018-03-15 10:58:09 +0100

    'Add'

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<kulek>:<zaq12wsx>@ds113799.mlab.com:13799/nodeappdatabase', {
+mongoose.connect('mongodb://kulek:123@ds113799.mlab.com:13799/nodeappdatabase', {
     
 });
 
